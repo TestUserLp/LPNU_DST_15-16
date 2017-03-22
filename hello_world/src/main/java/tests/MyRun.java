c6603111e335b95474b01c816c43e0e273d8991e
TestUserLp <orest.a.lavriv@lpnu.ua>
2017-03-22 16:33:50 +0200

    no message

--- hello_world/src/main/java/tests/MyRun.java
+++ hello_world/src/main/java/tests/MyRun.java
@@ -1,17 +1,9 @@
 package tests;
 
 public class MyRun {
-	public static void main(String[] args) {
-		String[][] argCopy = new String[2][2];
-		int x;
-		argCopy[0] = args;
-		x = argCopy[0].length;
-		for (int y = 0; y < x; y++) {
-			System.out.print(" " + argCopy[0][y]);
-		}
-		System.out.print(" ");
-		for(String s: argCopy[0]){
-			System.out.print(s+" ");
-		}
-	}
+public static void main(String [] args) {
+int x = 10;
+for (int y=0; y<5; y++, x--)
+System.out.print(x + ", ");
+}
 }
